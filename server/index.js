45ce38374f300884ce5f8088c37e21dd6d57f658
Sergey Gornostaev <sergey@gornostaev.pro>
2017-11-30 20:11:42 +0200

    browser-policy

--- server/index.js
+++ server/index.js
@@ -1,3 +1,9 @@
-import '/server/account';
+import { Meteor } from 'meteor/meteor';
+import { BrowserPolicy } from 'meteor/browser-policy';
 
+import '/server/account';
 import '/imports/startup/server/index';
+
+Meteor.startup(() => {
+  BrowserPolicy.framing.allowAll();
+});
